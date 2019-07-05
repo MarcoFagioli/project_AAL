06b22fe4cc2bf44baa4bdc0f02f67aa75d730511
Pietro Colombo <pietrocolombo@me.com>
2019-07-05 14:15:53 +0200

    rete salvata c'è qualche problema con la gui

--- Bayesian/script_for_gui.r
+++ Bayesian/script_for_gui.r
@@ -22,36 +22,39 @@ class <- c("sitting", "sittingdown", "standing", "standingup", "walking")
 row <- test_fit[1,]
 real_class <- row$class
 
-roll1 <- row$roll1_disc
-roll2 <- row$roll2_disc
-roll3 <- row$roll3_disc
-pitch3 <- row$pitch3_disc
-pitch4 <- row$pitch4_disc
 accel1 <- row$accel1_disc
 accel2 <- row$accel2_disc
+accel3 <- row$accel3_disc
+accel4 <- row$accel4_disc
+roll2 <- row$roll2_disc
+roll3 <- row$roll3_disc
+pitch2 <- row$pitch2_disc
+pitch3 <- row$pitch3_disc
+pitch4 <- row$pitch4_disc
+
 accel_mean <- row$accel_mean_disc
 accel_std <- row$accel_std_disc
 
 
-prob_sitting <- cpquery(fit, sitting == 1, roll1_disc == roll1 & roll2_disc == roll2 & roll3_disc == roll3 & 
-                           pitch3_disc == pitch3 & pitch4_disc == pitch4 & accel1_disc == accel1 &
-                           accel2_disc == accel2 & accel_mean_disc == accel_mean & accel_std_disc == accel_std)
+prob_sitting <- cpquery(fit, sitting == 1, accel1_disc == accel1 & accel2_disc == accel2 & accel3_disc == accel3 & accel4_disc == accel4 &
+                           roll2_disc == roll2 & roll3_disc == roll3 & pitch2_disc == pitch2 & pitch3_disc == pitch3 & pitch4_disc == pitch4 &
+                           accel_mean_disc == accel_mean & accel_std_disc == accel_std)
 
-prob_sittingdown <- cpquery(fit, sittingdown == 1, roll1_disc == roll1 & roll2_disc == roll2 & roll3_disc == roll3 & 
-                               pitch3_disc == pitch3 & pitch4_disc == pitch4 & accel1_disc == accel1 &
-                               accel2_disc == accel2 & accel_mean_disc == accel_mean & accel_std_disc == accel_std)
+prob_sittingdown <- cpquery(fit, sittingdown == 1, accel1_disc == accel1 & accel2_disc == accel2 & accel3_disc == accel3 & accel4_disc == accel4 &
+                               roll2_disc == roll2 & roll3_disc == roll3 & pitch2_disc == pitch2 & pitch3_disc == pitch3 & pitch4_disc == pitch4 &
+                               accel_mean_disc == accel_mean & accel_std_disc == accel_std)
 
-prob_standing <- cpquery(fit, standing == 1, roll1_disc == roll1 & roll2_disc == roll2 & roll3_disc == roll3 & 
-                            pitch3_disc == pitch3 & pitch4_disc == pitch4 & accel1_disc == accel1 &
-                            accel2_disc == accel2 & accel_mean_disc == accel_mean & accel_std_disc == accel_std)
+prob_standing <- cpquery(fit, standing == 1, accel1_disc == accel1 & accel2_disc == accel2 & accel3_disc == accel3 & accel4_disc == accel4 &
+                            roll2_disc == roll2 & roll3_disc == roll3 & pitch2_disc == pitch2 & pitch3_disc == pitch3 & pitch4_disc == pitch4 &
+                            accel_mean_disc == accel_mean & accel_std_disc == accel_std)
 
-prob_standingup <- cpquery(fit, standingup == 1, roll1_disc == roll1 & roll2_disc == roll2 & roll3_disc == roll3 & 
-                              pitch3_disc == pitch3 & pitch4_disc == pitch4 & accel1_disc == accel1 &
-                              accel2_disc == accel2 & accel_mean_disc == accel_mean & accel_std_disc == accel_std)
+prob_standingup <- cpquery(fit, standingup == 1, accel1_disc == accel1 & accel2_disc == accel2 & accel3_disc == accel3 & accel4_disc == accel4 &
+                              roll2_disc == roll2 & roll3_disc == roll3 & pitch2_disc == pitch2 & pitch3_disc == pitch3 & pitch4_disc == pitch4 &
+                              accel_mean_disc == accel_mean & accel_std_disc == accel_std)
 
-prob_walking <- cpquery(fit, walking == 1, roll1_disc == roll1 & roll2_disc == roll2 & roll3_disc == roll3 & 
-                           pitch3_disc == pitch3 & pitch4_disc == pitch4 & accel1_disc == accel1 &
-                           accel2_disc == accel2 & accel_mean_disc == accel_mean & accel_std_disc == accel_std)
+prob_walking <- cpquery(fit, walking == 1, accel1_disc == accel1 & accel2_disc == accel2 & accel3_disc == accel3 & accel4_disc == accel4 &
+                           roll2_disc == roll2 & roll3_disc == roll3 & pitch2_disc == pitch2 & pitch3_disc == pitch3 & pitch4_disc == pitch4 &
+                           accel_mean_disc == accel_mean & accel_std_disc == accel_std)
 
 prob <- c(prob_sitting, prob_sittingdown, prob_standing, prob_standingup, prob_walking)
 
